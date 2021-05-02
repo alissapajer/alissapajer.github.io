8026b74f7d18cf6c68e52cbd87c5f5b1295a4a4c
Alissa Pajer <alissa.pajer@gmail.com>
2021-05-01 20:48:24 -0400

    postssss

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,6 +5,15 @@ FIRST_NAME='Alissa'
 LAST_NAME='Blog'
 SITEDESCRIPTION="this is a worb site"
 OUTPUT_PATH="docs/"
+ARTICLE_URL="posts/{slug}.html"
+ARTICLE_SAVE_AS="posts/{slug}.html"
+
+#PLUGINS=['page-hierarchy']
+
+# https://github.com/akhayyat/pelican-page-hierarchy
+#PAGE_URL = '{slug}/'
+#PAGE_SAVE_AS = '{slug}/index.html'
+#SLUGIFY_SOURCE = 'basename'
 
 #PLUGINS=['pelican_katex']
 #KATEX={'output': 'mathml',
